cc090aea2168c22b227cd3f78ba975dbd97132c9
dmitriy kim <d.villarionovich@gmail.com>
2024-07-03 00:40:41 +0200

    created models for borrowings service

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,3 +1,67 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+from books_service.models import Book
+
+PAYMENTS_CHOICE = [
+    ("Pn", "Pending"),
+    ("Pd", "Paid"),
+]
+
+PAYMENT_TYPE = [
+    ("P", "PAYMENT"),
+    ("F", "FINE"),
+]
+
+
+class Borrowings(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Borrowings"
+
+    def __str__(self):
+        return f"{self.pk} |Book: {self.book.title}, Borrowed at {str(self.borrow_date)} - {self.user.email}"
+
+    def validate_book(self):
+        if self.book.inventory == 0:
+            raise ValidationError({"no_book": f"there are no books in the inventory!"})
+
+    def clean(self):
+        super().clean()
+        self.validate_book()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+class Payment(models.Model):
+    status = models.CharField(choices=PAYMENTS_CHOICE, max_length=2)
+    type = models.CharField(choices=PAYMENT_TYPE, max_length=2)
+    borrowing_id = models.ForeignKey(Borrowings, on_delete=models.CASCADE)
+    session_url = models.URLField()
+    session_id = models.IntegerField()
+
+    @property
+    def money_to_pay(self):
+        return self.get_charge()
+
+    def __str__(self):
+        return f"payment: {self.status}, {self.type}, {self.borrowing_id}"
+
+    def get_charge(self):
+        if self.borrowing_id.actual_return_date:
+            borrow_date = self.borrowing_id.borrow_date
+            return_date = self.borrowing_id.actual_return_date
+            daily_fee = self.borrowing_id.book.daily_fee
+            period = (return_date - borrow_date).days
+            charge = daily_fee * period
+
+            return charge
+        return 0
