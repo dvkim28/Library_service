e64497ed8f5fc5a367706f20baba75b2c523bb36
dmitriy kim <d.villarionovich@gmail.com>
2024-07-02 17:28:28 +0200

    created models for book

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,26 @@
+import decimal
+
 from django.db import models
 
-# Create your models here.
+COVER_CHOICE = {
+    ("H", "Hard"),
+    ("S", "Soft"),
+}
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(
+        max_length=100,
+        choices=COVER_CHOICE,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+
+    def __str__(self):
+        return self.title
